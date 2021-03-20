e9fb6b1489f3420533353ec9a2060c9a541e2ac7
Zoe Martin <git@zoemartin.me>
2021-03-20 04:51:14 +0100

    Fixed Uptime

--- src/main/java/me/zoemartin/rubie/modules/baseCommands/About.java
+++ src/main/java/me/zoemartin/rubie/modules/baseCommands/About.java
@@ -5,6 +5,7 @@ import me.zoemartin.rubie.core.CommandEvent;
 import me.zoemartin.rubie.core.annotations.Command;
 import me.zoemartin.rubie.core.annotations.CommandOptions;
 import me.zoemartin.rubie.core.interfaces.AbstractCommand;
+import me.zoemartin.rubie.core.util.TimeUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
 import org.joda.time.*;
 import org.joda.time.format.PeriodFormatter;
@@ -12,7 +13,9 @@ import org.joda.time.format.PeriodFormatterBuilder;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.temporal.ChronoUnit;
 import java.util.Enumeration;
+import java.util.concurrent.TimeUnit;
 import java.util.jar.Manifest;
 
 @Command
@@ -42,17 +45,7 @@ public class About extends AbstractCommand {
         eb.addField("Bot Version", version == null ? "DEV BUILD" : version, true);
         eb.addField("Java Version", System.getProperty("java.version"), true);
         eb.addField("JDA Version", JDA_VERSION, true);
-        PeriodFormatter formatter = new PeriodFormatterBuilder()
-                                        .appendDays()
-                                        .appendSuffix(" days, ")
-                                        .appendHours()
-                                        .appendSuffix(" hours, ")
-                                        .appendMinutes()
-                                        .appendSuffix(" minutes, ")
-                                        .appendSeconds()
-                                        .appendSuffix(" seconds")
-                                        .toFormatter();
-        eb.addField("Uptime", formatter.print(new Duration(STARTUP, DateTime.now()).toPeriodFrom(STARTUP)), true);
+        eb.addField("Uptime", format(STARTUP, DateTime.now()), true);
         Runtime rt = Runtime.getRuntime();
         long usedMB = (rt.totalMemory() - rt.freeMemory()) / 1024 / 1024;
         eb.addField("Memory Usage", usedMB + " MB", true);
@@ -64,6 +57,44 @@ public class About extends AbstractCommand {
             "https://raw.githubusercontent.com/DV8FromTheWorld/JDA/assets/assets/readme/logo.png");
 
         event.getChannel().sendMessage(eb.build()).queue();
+    }
+
+    private static String format(DateTime s, DateTime e) {
+        // TODO: Refactor into TimeUtils
+        var d = new Duration(s, e);
+        var p = d.toPeriodFrom(s);
+
+        var f1 = new PeriodFormatterBuilder()
+                     .appendYears()
+                     .appendSuffix(p.getYears() > 1 ? " years " : " year ")
+                     .appendMonths()
+                     .appendSuffix(p.getMonths() > 1 ? " months " : " month ")
+                     .appendWeeks()
+                     .appendSuffix(p.getWeeks() > 1 ? " weeks " : " week ")
+                     .appendPrefix(d.getMillis() > TimeUnit.DAYS.toMillis(7) ? " and " : "")
+                     .appendDays()
+                     .appendSuffix(p.getDays() > 1 ? " days " : " day ")
+                     .toFormatter();
+
+        var f2 = new PeriodFormatterBuilder()
+                     .appendWeeks()
+                     .appendSuffix(p.getWeeks() > 1 ? " weeks " : " week ")
+                     .appendDays()
+                     .appendSuffix(p.getDays() > 1 ? " days " : " day ")
+                     .appendHours()
+                     .appendSuffix(p.getHours() > 1 ? " hours " : " hour ")
+                     .appendMinutes()
+                     .appendSuffix(p.getMinutes() > 1 ? " minutes " : " minute ")
+                     .appendPrefix(d.getMillis() > TimeUnit.MINUTES.toMillis(1) ? " and " : "")
+                     .appendSeconds()
+                     .appendSuffix(p.getSeconds() > 1 ? " seconds" : " second")
+                     .toFormatter();
+
+        System.out.println(d.getMillis());
+        System.out.println(TimeUnit.DAYS.toMillis(7));
+
+        if (d.getMillis() >= TimeUnit.DAYS.toMillis(7)) return f1.print(p);
+        else return f2.print(p);
     }
 
     private void findVersion() {
